9b0e2ba2612bf5cfb16476f1a4233ec8daab895b
Vinicius-Guerra <vini.fms@hotmail.com>
2022-04-28 15:00:18 -0300

    Page 3 e scroll
    Finalizando a page 3 e adicionando effectts ao scroll da page

--- main.js
+++ main.js
@@ -17,3 +17,18 @@ for(const link of links) {
         nav.classList.remove('show')
     })
 }
+
+/* mudar header da page quando der scroll */
+
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        // scroll maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        //menor que a altura do header
+        header.classList.remove('scroll')
+    }
+})
